d090465a4b624b732871c992adb14111fe21fdba
Guhxs1010 <133364786+Guhxs1010@users.noreply.github.com>
2024-04-12 14:37:12 -0300

    Add files via upload

---
+++ exercicio1/exercicio1/exercicio1.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+/*using namespace std;
+int main() {
+	int instalacao = 300;
+	int limpeza = 150;
+	int manutencao = 499;
+	 
+	int soma = instalacao + limpeza + manutencao;
+
+	cout << soma;
+
+	return 0;
+}*/
+
+/*using namespace std;
+int main() {
+	int receber = 35;
+	int horas = 8;
+	
+	int mult = receber * horas;
+
+	cout << mult;
+	return 0;
+}*/
+
+/*using namespace std;
+int main() {
+	int unidade = 27;
+	int pamela = 16;
+
+	int sub = unidade - pamela;
+
+	cout << sub;
+	return 0;
+}*/
+
+/*using namespace std;
+int main() {
+	int garafas = 300;
+	int caixa = 20;
+
+	int div = garafas / caixa;
+
+	cout << div;
+	return 0;
+}*/
+
+/*using namespace std;
+int main() {
+	int camiseta = 120;
+	int vendeu = 80;
+	int valor = 30;
+	int comprou = 50;
+
+	int totalcamisa = camiseta - vendeu + comprou;
+	int mult = vendeu * valor;
+
+	cout << totalcamisa;
+	cout << "\n";
+	cout << mult;
+
+	return 0;
+
+}*/
+
+
